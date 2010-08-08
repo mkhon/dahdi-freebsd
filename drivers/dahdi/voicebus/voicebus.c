dbea893d8ae3b5673cfb8e9794f7ba0182273246
fjoe <fjoe@a0bf4364-ded3-4de4-8d8a-66a801d63aff>
2010-08-08 08:33:41 +0000

    Cosmetic fix: define MODULE_VERSION after DAHDI_DEV_MODULE.

--- drivers/dahdi/voicebus/voicebus.c
+++ drivers/dahdi/voicebus/voicebus.c
@@ -2101,8 +2101,8 @@ dahdi_voicebus_modevent(module_t mod __unused, int type, void *data __unused)
 	}
 }
 
-MODULE_VERSION(dahdi_voicebus, 1);
 DAHDI_DEV_MODULE(dahdi_voicebus, dahdi_voicebus_modevent, NULL);
+MODULE_VERSION(dahdi_voicebus, 1);
 MODULE_DEPEND(dahdi_voicebus, dahdi, 1, 1, 1);
 #else /* !__FreeBSD__ */
 MODULE_DESCRIPTION("Voicebus Interface w/VPMADT032 support");
