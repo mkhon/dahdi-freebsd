ffef5bad51e69a7e19be3ae0b1dc915e8dde38a7
fjoe <fjoe@a0bf4364-ded3-4de4-8d8a-66a801d63aff>
2010-08-09 10:49:34 +0000

    Fix D_VERSION_LINEAR definition.

--- include/dahdi/compat/bsd.h
+++ include/dahdi/compat/bsd.h
@@ -16,7 +16,7 @@
 #include <machine/atomic.h>
 #include <machine/bus.h>
 
-#define D_VERSION_LINEAR	((D_VERSION & 0xffff) | ((D_VERSION >> 16) & 0xff) | ((D_VERSION >> 24) & 0xff))
+#define D_VERSION_LINEAR	(((D_VERSION & 0xffff) << 16) | (((D_VERSION >> 16) & 0xff) << 8) | ((D_VERSION >> 24) & 0xff))
 
 #define LINUX_VERSION_CODE	KERNEL_VERSION(2, 6, 32)
 #define KERNEL_VERSION(x, y, z)	(((x) << 16) + ((y) << 8) + (z))
