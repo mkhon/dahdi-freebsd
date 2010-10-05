f52d5c4920bd3cc89d79df969a43da3c35991876
fjoe <fjoe@a0bf4364-ded3-4de4-8d8a-66a801d63aff>
2010-10-05 16:47:16 +0000

    Define __BIG_ENDIAN and __LITTLE_ENDIAN properly. This may fix wctdm on sparc64.

--- include/dahdi/compat/bsd.h
+++ include/dahdi/compat/bsd.h
@@ -66,9 +66,11 @@
 #define __be32  u_int32_t
 
 #if _BYTE_ORDER == _LITTLE_ENDIAN
+#define __LITTLE_ENDIAN
 #define __LITTLE_ENDIAN_BITFIELD
 #define __constant_htons(x)	((uint16_t) (((uint16_t) (x)) << 8 | ((uint16_t) (x)) >> 8))
 #elif _BYTE_ORDER == _BIG_ENDIAN
+#define __BIG_ENDIAN
 #define __BIG_ENDIAN_BITFIELD
 #define __constant_htons(x)	(x)
 #endif
