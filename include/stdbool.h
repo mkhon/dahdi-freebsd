5c9a8a8e0d0485f72ddda53a3b14cdcd3f0f2fb9
fjoe <fjoe@a0bf4364-ded3-4de4-8d8a-66a801d63aff>
2012-02-23 07:43:07 +0000

    Fix build on FreeBSD 9 and later where "bool" is defined in <sys/types.h>

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,11 +1,15 @@
 #ifndef _STDBOOL_H_
 #define _STDBOOL_H_
 
+#include <sys/types.h>
+
+#ifndef __bool_true_false_are_defined
 typedef int bool;
 
 enum {
 	false,
 	true
 };
+#endif
 
 #endif /* _STDBOOL_H_ */
