f560f02a8b74d95e3085a86c4da6ed2335e72525
fjoe <fjoe@a0bf4364-ded3-4de4-8d8a-66a801d63aff>
2014-05-10 08:11:57 +0000

    Unbreak on FreeBSD 11 (no AppleTalk and IPX support).

--- bsd-kmod/dahdi/ng_dahdi_iface.c
+++ bsd-kmod/dahdi/ng_dahdi_iface.c
@@ -281,10 +281,14 @@ dahdi_iface_create(struct dahdi_chan *chan)
 	/* connect other hooks */
 	dahdi_iface_connect_node_path(node, DAHDI_IFACE_HOOK_UPPER,
 	    NG_CISCO_HOOK_INET, NG_CISCO_HOOK_INET6, NG_IFACE_HOOK_INET6);
+#if __FreeBSD_version < 1100013
 	dahdi_iface_connect_node_path(node, DAHDI_IFACE_HOOK_UPPER,
 	    NG_CISCO_HOOK_INET, NG_CISCO_HOOK_APPLETALK, NG_IFACE_HOOK_ATALK);
+#endif
+#if __FreeBSD_version < 1100012
 	dahdi_iface_connect_node_path(node, DAHDI_IFACE_HOOK_UPPER,
 	    NG_CISCO_HOOK_INET, NG_CISCO_HOOK_IPX, NG_IFACE_HOOK_IPX);
+#endif
 
 	/* get iface name */
 	NG_MKMESSAGE(msg, NGM_IFACE_COOKIE, NGM_IFACE_GET_IFNAME, 0, M_WAITOK);
