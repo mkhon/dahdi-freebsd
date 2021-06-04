908233ee73885cafd892237bd3f79556dd1d71b2
Joan Touzet <wohali@users.noreply.github.com>
2021-06-03 21:47:04 -0400

    New include for crc32
    https://reviews.freebsd.org/rS349151 moved the `crc32` functionality to `gsb_crc32.h`.

--- include/linux/crc32.h
+++ include/linux/crc32.h
@@ -1,6 +1,9 @@
 #ifndef _LINUX_CRC32_H_
 #define _LINUX_CRC32_H_
 
+#if defined(__FreeBSD__)
+#include <sys/gsb_crc32.h>
+#endif
 #include <sys/libkern.h>
 
 #define crc32_le(crc, data, len)	crc32_raw(data, len, crc)
